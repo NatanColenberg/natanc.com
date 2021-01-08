124d81a14e35d01e2684b7cce09b8f0f5a44fbd8
NatanColenberg <natan.colenberg@gmail.com>
2021-01-08 22:37:06 +0200

    some cool stuff 2

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -3,9 +3,6 @@ import "./intro.css";
 
 export default function Intro() {
   const [showTextCursor, setShowTextCursor] = useState(false);
-  const [sentence, setSentence] = useState("");
-  const [typedWordsIndex, setTypedWordsIndex] = useState(0);
-  const typedSentence = "and I LOVE programming";
 
   useEffect(() => {
     setTimeout(() => {
@@ -13,28 +10,11 @@ export default function Intro() {
     }, 500);
   }, [showTextCursor]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (typedWordsIndex >= typedSentence.length) {
-        return;
-      }
-      let newSentence = sentence + typedSentence[typedWordsIndex];
-      setTypedWordsIndex(typedWordsIndex + 1);
-      setSentence(newSentence);
-    }, 200);
-  }, [sentence]);
-
   return (
     <div className="introWrap">
       <h1>Hey There,</h1>
       <h1>
         I'm <span className="introName"> Natan</span>;)
-        {/* {sentence && (
-          <span className="introSentence">
-            <br />
-            {sentence}
-          </span>
-        )} */}
         {showTextCursor && <span className="introTextCursor">▌</span>}
       </h1>
     </div>
